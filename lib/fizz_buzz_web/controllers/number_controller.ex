9848c4defe92e64578547c0faa6290e7cbab117c
mariaclrd <mariaclrd@gmail.com>
2018-01-27 18:53:24 +0000

    moving default pagination to the controller

--- lib/fizz_buzz_web/controllers/number_controller.ex
+++ lib/fizz_buzz_web/controllers/number_controller.ex
@@ -2,17 +2,25 @@ defmodule FizzBuzzWeb.NumberController do
   use FizzBuzzWeb, :controller
   alias FizzBuzz.Numbers.Number
 
+  @pagination_defaults %{"current_page" => 1, "page_size" =>100 }
+
   def index(conn, %{"format" => "json"} = params) do
+    params = merge_pagination_defaults(params)
     render(conn, "index.json", %{numbers: Number.all,
       current_page: params["current_page"],
       page_size: params["page_size"]})
   end
 
   def index(conn, params) do
+    params = merge_pagination_defaults(params)
     conn
     |> assign(:numbers, Number.all)
     |> assign(:current_page, params["current_page"])
     |> assign(:page_size, params["page_size"])
-    |> render :index
+    |> render(:index)
+  end
+
+  defp merge_pagination_defaults(args) do
+    Map.merge(@pagination_defaults, args, fn _key, default, val -> val || default end)
   end
 end
--- lib/fizz_buzz_web/views/number_view.ex
+++ lib/fizz_buzz_web/views/number_view.ex
@@ -3,18 +3,15 @@ defmodule FizzBuzzWeb.NumberView do
   alias FizzBuzz.Numbers.Pagination
   alias FizzBuzz.Numbers.FizzBuzzCalculator
 
-  @pagination_defaults %{current_page: 1, page_size: 100}
-
   def entries(args) do
     collection_as_json(args)[:entries]
   end
 
-  def render("index.json", args) do
-    collection_as_json(args)
+  def render("index.json", %{numbers: numbers, current_page: current_page, page_size: page_size}) do
+    collection_as_json(%{numbers: numbers, current_page: current_page, page_size: page_size})
   end
 
-  def collection_as_json(args) do
-    %{numbers: numbers, current_page: current_page, page_size: page_size} = merge_pagination_defaults(args)
+  def collection_as_json(%{numbers: numbers, current_page: current_page, page_size: page_size}) do
     %{
       entries: Pagination.entries(numbers, current_page, page_size) |> Enum.map(&as_json/1),
       pagination: pagination(numbers, page_size, current_page)
@@ -34,9 +31,5 @@ defmodule FizzBuzzWeb.NumberView do
       total_pages: Pagination.total_pages(numbers, page_size),
       current_page: current_page
     }
-  end
-
-  defp merge_pagination_defaults(args) do
-    Map.merge(@pagination_defaults, args, fn _key, default, val -> val || default end)
   end
 end
